49c19cf28a92047bf5ccfa15604309b97650344f
irinangela <iriniaggelikisoulele@gmail.com>
2023-05-31 19:17:38 +0300

    book

--- Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/__init__.py
+++ Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/__init__.py
@@ -4,6 +4,8 @@ from flask_mysqldb import MySQL
 from dbUI.login import login
 from dbUI.signup import signup
 from dbUI.student import student
+from dbUI.book import book
+from dbUI.bookinfo import bookinfo
 
 ## __name__ is the name of the module. When running directly from python, it will be 'dbdemo'
 ## Outside of this module, as in run.py, it is '__main__' by default
@@ -28,7 +30,10 @@ from dbUI import routes
 from dbUI.login import routes
 from dbUI.signup import routes
 from dbUI.student import routes
+from dbUI.book import routes
+from dbUI.bookinfo import routes
 app.register_blueprint(login)
 app.register_blueprint(signup)
 app.register_blueprint(student)
-
+app.register_blueprint(book)
+app.register_blueprint(bookinfo)
--- Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/student/routes.py
+++ Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/student/routes.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 from dbUI import db ## initially created by __init__.py, need to be used here
 from dbUI.student import student
 from dbUI.student.forms import StudentForm
+from dbUI.book import book
 
 @student.route("/student/<string:username>")
 def getStudent(username):
@@ -15,7 +16,7 @@ def getStudent(username):
         query = "SELECT b.title, r.r_date FROM reserves r INNER JOIN book b ON b.ISBN = r.ISBN WHERE r.username = '{}';".format(username)
         column_names = [i[0] for i in cur.description]
         reserves = [dict(zip(column_names, entry)) for entry in cur.fetchall()]
-        return render_template("student.html", borrows = borrows,reserves = reserves, pageTitle = "Student Page")
+        return render_template("student.html", borrows = borrows,reserves = reserves, username = username, pageTitle = "Student Page")
     except Exception as e:
         abort(500)    
 
