a5839e3908832ed878eddee47319f02938694cde
irinangela <iriniaggelikisoulele@gmail.com>
2023-06-03 00:18:15 +0300

    teacher page

--- Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/teacher/forms.py
+++ Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/teacher/forms.py
@@ -1,12 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, InputRequired, Optional
 
 ## when passed as a parameter to a template, an object of this class will be rendered as a regular HTML form
 ## with the additional restrictions specified for each field
 class TeacherForm(FlaskForm):
-    username = StringField(label = "username", validators = [InputRequired(message = "Username is a required field.")])
-    
-    password = StringField(label = "password", validators = [InputRequired(message = "Password is a required field.")])
+    username = StringField(label = "username" , validators=[Optional()])
 
-    submit = SubmitField("Login")
+    first_name = StringField(label = "first_name" , validators=[Optional()])
+
+    last_name = StringField(label = "last_name" , validators=[Optional()])
+    
+    age = IntegerField(label = 'age', validators=[Optional()])
+
+    password = IntegerField(label = "password" , validators=[Optional()])
+
+    submit = SubmitField("Submit Updates")
--- Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/teacher/routes.py
+++ Databases-Python-Demo-main/Databases-Python-Demo-main/dbUI/teacher/routes.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, flash, redirect, url_for, abo
 from flask_mysqldb import MySQL
 from dbUI import db ## initially created by __init__.py, need to be used here
 from dbUI.teacher import teacher
+from dbUI.teacher.forms import TeacherForm
+from dbUI import app
+from dbUI.book import book
 
 @teacher.route("/teacher/<string:username>")
 def getTeacher(username):
@@ -12,11 +15,82 @@ def getTeacher(username):
         column_names = [i[0] for i in cur.description]
         borrows = [dict(zip(column_names, entry)) for entry in cur.fetchall()]
         query = "SELECT b.title, r.r_date FROM reserves r INNER JOIN book b ON b.ISBN = r.ISBN WHERE r.username = '{}';".format(username)
+        cur.execute(query)
         column_names = [i[0] for i in cur.description]
         reserves = [dict(zip(column_names, entry)) for entry in cur.fetchall()]
-        return render_template("teacher.html", borrows = borrows,reserves = reserves, pageTitle = "Teacher Page")
+        query = ("SELECT Library_card FROM approves_user WHERE username = '{}';".format(username))
+        cur.execute(query)
+        result = cur.fetchone()
+
+        if result is not None:
+            library_card = result[0]
+            has = 1 if library_card == 1 else 0
+        else:
+            has = 0
+        print(library_card)
+        print(has)
+        cur.close()
+
+        return render_template("teacher.html", borrows = borrows, username = username, reserves = reserves, library_card=library_card, has=has,pageTitle = "Teacher Page")
     except Exception as e:
         abort(500)    
 
+@teacher.route("/teacher/profile/<string:username>", methods=['GET', 'POST'])
+def getTeacherProfile(username):
 
+    form = TeacherForm()
+
+    if(request.method == "POST" and form.validate_on_submit()):
+        teacherinfo = form.__dict__
+        if teacherinfo["first_name"].data:
+            try:
+                query = "UPDATE teacher_user SET teacher_first_name = '{}' WHERE Username = '{}';".format(
+                    teacherinfo["first_name"].data,
+                    username
+                    )
+                cur = db.connection.cursor()
+                cur.execute(query)
+                db.connection.commit()
+                cur.close()
+            except Exception as e:
+                abort(500)  
+
+        if teacherinfo["last_name"].data:
+            try:
+                query = "UPDATE teacher_user SET teacher_last_name = '{}' WHERE Username = '{}';".format(
+                    teacherinfo["last_name"].data,
+                    username
+                    )
+                cur = db.connection.cursor()
+                cur.execute(query)
+                db.connection.commit()
+                cur.close()
+            except Exception as e:
+                abort(500)  
+
+        if teacherinfo["password"].data:
+            try:
+                query = "UPDATE users SET U_password = '{}' WHERE Username = '{}';".format(
+                    teacherinfo["password"].data,
+                    username
+                    )
+                cur = db.connection.cursor()
+                cur.execute(query)
+                db.connection.commit()
+                cur.close()
+            except Exception as e:
+                abort(500)                  
+
+    try:
+        cur = db.connection.cursor()
+        query = "SELECT * FROM usernameview u INNER JOIN users us ON u.username = us.username WHERE u.username = '{}';".format(username)
+        cur.execute(query)
+        column_names = [i[0] for i in cur.description]
+        information = [dict(zip(column_names, entry)) for entry in cur.fetchall()]
+        cur.close()
+
+    except Exception as e:
+        abort(500)      
+
+    return render_template("teacher_profile.html", information=information[0], pageTitle="Teacher Profile", form = form)
 
